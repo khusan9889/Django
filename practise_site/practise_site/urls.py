310e6d50918614a1a6b5ae276081cd2bcd1b186d
khusan9889 <k.xusan_2003@mail.ru>
2022-02-03 00:28:29 +0500

    ORM models, working with db

--- practise_site/practise_site/urls.py
+++ practise_site/practise_site/urls.py
@@ -13,14 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from first_app.views import *
 
+#new imports
 from django.urls import path,include
 
+from django.conf.urls.static import static
+from practise_site import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first_app/',include('first_app.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
